964920cb28ff3cefb27ba218264060332c815d4b
Sujoy Kumar Roy <sujoykroy@gmail.com>
2020-02-22 23:33:43 +0530

    Test fix.

--- test/com/robocam/Socket/FloatSubCommandTest.java
+++ test/com/robocam/Socket/FloatSubCommandTest.java
@@ -10,15 +10,15 @@ public class FloatSubCommandTest {
         FloatSubCommand command = new FloatSubCommand();
         command.setSubCommand(FloatSubCommand.SPEED_FASTER);
         command.setValue(.5);
-        assertEquals("flt:h,.5", command.buildCommand());
+        assertEquals("flt:h,0.50000!", command.buildCommand());
     }
 
     @Test
     public void testParseCommand() {
         FloatSubCommand command = new FloatSubCommand();
-        command.parseCommand("flt:a,.6");
+        command.parseCommand("flt:a,.6!");
         assertEquals(
                 FloatSubCommand.STEER_STOP, command.getSubCommand());
-        assertEquals(0.6f, command.getValue(), 0);
+        assertEquals(0.6f, command.getValue(), 0.01);
     }
 }
