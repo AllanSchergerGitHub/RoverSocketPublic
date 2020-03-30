e5da2c4e9cb37391506ab12764cc1bae7492e7dc
Sujoy Kumar Roy <sujoykroy@gmail.com>
2020-03-31 01:23:46 +0530

    Socket server keeps a loose list of connected client sockets.

--- src/com/robocam/Socket/SocketServer.java
+++ src/com/robocam/Socket/SocketServer.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
@@ -22,6 +23,8 @@ public class SocketServer implements Runnable {
     private final ServerSocket mServerSocket;
     private final ExecutorService mExecutorService;
     private final ComPipe mComPipe;
+    
+    private final ArrayList<Socket> mConnectedSockets = new ArrayList<>();
 
     public SocketServer(int port, ComPipe comPipe) throws IOException {
         mServerSocket = new ServerSocket(port);
@@ -31,6 +34,10 @@ public class SocketServer implements Runnable {
         //Additional 2 threads is for safe operation
         mExecutorService = Executors.newFixedThreadPool(5);
         mComPipe = comPipe;
+    }
+    
+    public Socket[] getConnectedSockets() {
+        return mConnectedSockets.toArray(new Socket[mConnectedSockets.size()]);
     }
 
     @Override
@@ -49,9 +56,20 @@ public class SocketServer implements Runnable {
                 Logger.getLogger(SocketServer.class.getName()).log(Level.SEVERE, null, ex);
             }
             if (socket != null) {
+                mConnectedSockets.add(socket);
                 System.out.println("New connection!" + socket.getRemoteSocketAddress().toString());
                 mExecutorService.execute(new SocketReadHandler(socket, mComPipe));
                 mExecutorService.execute(new SocketWriteHandler(socket, mComPipe));
+            }
+            
+            // Remove closed sockets
+            int i = 0;
+            while (i < mConnectedSockets.size()) {
+                if (mConnectedSockets.get(i).isClosed()) {
+                    mConnectedSockets.remove(i);
+                    i--;
+                }
+                i++;
             }
         }
     }
