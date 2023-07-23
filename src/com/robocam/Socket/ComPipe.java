ce69c1e1e362da14548dcc5990f6047e88184289
Allan Scherger <mturktest123@gmail.com>
2023-07-23 13:34:20 -0500

    sync public version

--- src/com/robocam/Socket/ComPipe.java
+++ src/com/robocam/Socket/ComPipe.java
@@ -48,9 +48,21 @@ public class ComPipe {
         return mInput.poll();
     }
 
+    /**
+     * The getOut() method is used to retrieve a message from the mOutput queue. 
+     * If the queue is empty and a certain amount of time has passed since the 
+     * last message was sent, a heartbeat message is returned instead.
+     * The getOut() method also updates the mLastMessageSentAt timestamp 
+     * each time a message is retrieved.
+     * The actual sending of the message is likely handled by the part of your 
+     * application that calls getOut(). This could be a separate thread or a 
+     * network-related class that's responsible for sending messages over a network connection.
+     * It appears like it could be either the SocketHandler or SocketWriteHandler classes.
+     * @return 
+     */
     public String getOut() {
         if (!mIsOpen) {
-            //System.out.println("null issue");
+            System.out.println("null issue? ComPipe isn't open?");
             return null;
         }
         String message = mOutput.poll();
@@ -65,11 +77,29 @@ public class ComPipe {
         return message;
     }
 
+    /**
+     * The putOut method is used to add a message to the mOutput queue. 
+     * This queue is used to store messages that are waiting to be sent.
+     * the offer() method in Java's Queue interface is used to insert a specific 
+     * element into the queue. This method is generally preferable to add(), 
+     * because offer() will not throw an exception if the queue is full or 
+     * capacity-restricted. Instead, it will return false.
+     * In the context of your code, mOutput.offer(message) is adding the message 
+     * to the mOutput queue. If the queue is full and cannot accept any more elements, 
+     * this operation will fail silently (i.e., it will not add the element, but 
+     * it also will not throw an exception).
+     * @param message 
+     */
     public void putOut(String message) {
         if (!mIsOpen || message == null) return;        
         mOutput.offer(message);
     }
 
+    /**
+     * The putIn method is used to add a message to the mInput queue. 
+     * This queue is used to store messages that have been received and are waiting to be processed.
+     * @param message 
+     */
     public void putIn(String message) {
         if(!message.equals(HEART_BEAT)) {
             //System.out.println("putIn message" + message);
@@ -85,6 +115,10 @@ public class ComPipe {
             } else {
                 //System.out.println("putIn message2 " + message);
             }
+            if(mInput.size()>100){
+                System.out.println("MAX_QUEUE_SIZE has exceeded 100. This is in ComPipe.java in the RoverSocketLib code: " + mInput.size() + " most recent message: " + message);
+            }
+            
             mInput.offer(message);
         }
     }
--- src/com/robocam/Socket/CommonSensorCommand.java
+++ src/com/robocam/Socket/CommonSensorCommand.java
@@ -3,7 +3,8 @@ package com.robocam.Socket;
 public class CommonSensorCommand extends Command {
     private static final String COMMAND_NAME = "cs";
     private double mElectricalCurrent;
-    
+    private double mDistanceRemainingRover = 0.0;
+
     public CommonSensorCommand() {
         super(COMMAND_NAME);
     }
@@ -16,20 +17,37 @@ public class CommonSensorCommand extends Command {
         mElectricalCurrent = value;
     }
     
+    public double getDistanceRemainingRover() {
+        return mDistanceRemainingRover;
+    }
+    
+    public void setDistanceRemainingRover(double value) {
+        mDistanceRemainingRover = value;
+    }
+    
     @Override
     public String buildCommand() {
-        return String.format(
-            "%s%s%f%s",
+        return String.format("%s%s%f%s%f%s",
             COMMAND_NAME, Command.COMMAND_ARG_SEPARATOR,
-            mElectricalCurrent,
+            mElectricalCurrent, Command.COMMAND_ARG_SEPARATOR,
+            mDistanceRemainingRover,
             PRIORITY_COMMAND_FLAG);
     }
 
     @Override
+    /**
+     * An example command looks like this: cs:-0.645920:120.750000!
+     * In this example the electricalcurrent is -0.645 and the distanceremaining is 120.75
+     * This data is used on the UI side.
+     */
     public void parseCommand(String command) {
         String[] arguments = getArguments(command);
         if (arguments.length > 0) {
-            mElectricalCurrent = Double.parseDouble(arguments[0].trim());
+            String[] numbers = arguments[0].trim().split(":");
+            if (numbers.length == 2) {
+                mElectricalCurrent = Double.parseDouble(numbers[0]);
+                mDistanceRemainingRover = Double.parseDouble(numbers[1]);
+            }
         }
     }
 }
